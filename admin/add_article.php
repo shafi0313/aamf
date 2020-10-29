e8f0d329499fc5c90651a037c763eaf327b4786b
Shafi <msh.shafiul@gmail.com>
2020-10-29 14:23:49 +0600

    Work on artiale

--- admin/add_article.php
+++ admin/add_article.php
@@ -25,23 +25,23 @@
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        <div class="row"> 
+        <div class="row">
           <!-- right column -->
-          <div class="col-md-12">            
+          <div class="col-md-12">
             <!-- general form elements disabled -->
             <div class="card card-warning">
-              <div class="card-header">                
+              <div class="card-header">
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-<?php 
+<?php
   if (isset($_POST['submit'])) {
       $article_title      = v($_POST['article_title']);
       $article_text       =   $_POST['article_text'];
       $article_author     = v($_POST['article_author']);
       $article_date       = v($_POST['article_date']);
       $article_image      = v($_FILES['image']['name']);
-      $article_image_temp = $_FILES['image']['tmp_name']; 
+      $article_image_temp = $_FILES['image']['tmp_name'];
 
       move_uploaded_file($article_image_temp, "../image/article_image/$article_image");
 
@@ -52,7 +52,7 @@
           $query = "INSERT INTO post_article (article_title, article_text, article_author, article_date, article_image) ";
           $query .= "VALUES ('{$article_title}','{$article_text}','{$article_author}','{$article_date}','{$article_image}')";
           $create_article = mysqli_query($cont, $query);
-          
+
           if($create_article) {
              echo "<div class='btn btn-primary btn-block'>Data Inserted Successfully</div><br>";
              header("Location: post_article.php");
@@ -60,10 +60,10 @@
 
           if(!$create_article) {
               die('QUERY FAILDE' . mysqli_error($cont));
-          }                
+          }
         }
     }
-?>                
+?>
     <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
         <div class="form-group">
             <label for="">Title of Article*</label>
@@ -74,17 +74,17 @@
           <label for="">Article Content*</label>
           <div class="form-group">
             <textarea class="textarea" name="article_text" placeholder="Type Article" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-          </div>              
+          </div>
         </div>
-        
+
         <div class="form-group">
             <label for="">Name of Author*</label>
             <input class="form-control" type="text" name="article_author" placeholder="Enter Author Name">
-        </div>                    
+        </div>
         <div class="form-group">
             <label for="">Date*</label>
             <input class="form-control" style="max-width: 180px;" type="date" name="article_date">
-        </div>                    
+        </div>
         <div class="form-group">
             <label for="">Upload Image (max size : 1MB &amp; W:400px; H:200px)</label>
             <input type="file" name="image" id="name">
@@ -93,11 +93,11 @@
         <button class="btn btn-danger" type="reset">Reset</button>
     </form>
 
-<?php 
+<?php
     function v($post){
         $post = trim($post);
         $post = stripcslashes($post);
-        $post = htmlspecialchars($post);        
+        $post = htmlspecialchars($post);
         return $post;
     }
  ?>
@@ -124,9 +124,9 @@
 <!-- ./wrapper -->
 
 <?php
-  include 'includes/footer.php';      
+  include 'includes/footer.php';
 ?>
-  
+
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
